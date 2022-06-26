a8fa880533428b8edbb3d9b406399b2824dc7d53
ohshitnotgood <thumbs.exile_0g@icloud.com>
2022-06-26 17:23:22 +0600

    Changed accentColor and added navBar buttons

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,10 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selectionMode: Int = 0
+    @State private var selectionMode    : Int    = 0
+    @State private var searchText       : String = ""
+    
+    @State private var showItemsView    : Bool = false
+    @State private var showSettingsView : Bool = false
+    
     
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            List {
+                
+            }.navigationTitle("Orders")
+                .sheet(isPresented: $showItemsView, content: { ItemsView() })
+                .sheet(isPresented: $showSettingsView, content: { SettingsView() })
+                .toolbar {
+                    
+                    // MARK: Delete button
+                    ToolbarItem(placement: .bottomBar, content: {
+                        Button(action: {
+                            showItemsView = true
+                        }) {
+                            Image(systemName: "trash")
+                        }
+                    })
+                    
+                    // MARK: New Item button
+                    ToolbarItem(placement: .bottomBar, content: {
+                        Button(action: {
+                            showItemsView = true
+                        }) {
+                            Image(systemName: "square.and.pencil")
+                        }
+                    })
+                    
+                    // MARK: Settings button
+                    ToolbarItem(placement: .navigationBarTrailing, content: {
+                        Button(action: {
+                            showSettingsView = true
+                        }) {
+                            Image(systemName: "gearshape")
+                        }
+                    })
+                }
+        }
     }
 }
 
