032f765591e71ffbf31f33b0c7a3ca51ab59bc38
ohshitnotgood <thumbs.exile_0g@icloud.com>
2022-06-26 17:17:15 +0600

    Added SettingsView

---
+++ Shared/Settings/SettingsView.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsView.swift
+//  Bubble (iOS)
+//
+//  Created by Praanto Samadder on 26/06/2022.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                
+            }.navigationTitle("Settings")
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
