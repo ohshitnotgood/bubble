c2a1b39ad21a77cdaa3cc9f6630d6cfe631cbda8
ohshitnotgood <thumbs.exile_0g@icloud.com>
2022-06-27 16:34:08 +0600

    Created FileManager

---
+++ Shared/Services/Storage/FileManager.swift
@@ -0,0 +1,14 @@
+//
+//  FileManager.swift
+//  Bubble (iOS)
+//
+//  Created by Praanto Samadder on 27/06/2022.
+//
+
+import Foundation
+
+extension FileManager {
+    func store() {
+        
+    }
+}
