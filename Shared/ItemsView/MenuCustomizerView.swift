85631160246593dd942d19b33ffbe8c5309e655f
ohshitnotgood <thumbs.exile_0g@icloud.com>
2022-06-27 08:34:06 +0600

    Added MenuCustomizerView

---
+++ Shared/ItemsView/MenuCustomizerView.swift
@@ -0,0 +1,22 @@
+//
+//  MenuCustomizerView.swift
+//  Bubble (iOS)
+//
+//  Created by Praanto Samadder on 27/06/2022.
+//
+
+import SwiftUI
+
+struct MenuCustomizerView: View {
+    var body: some View {
+        List {
+            
+        }
+    }
+}
+
+struct MenuCustomizerView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuCustomizerView()
+    }
+}
