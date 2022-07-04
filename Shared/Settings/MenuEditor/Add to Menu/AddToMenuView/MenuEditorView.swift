1ce9809c0f2c144f515cd040b12702775e71e867
ohshitnotgood <thumbs.exile_0g@icloud.com>
2022-07-04 20:10:46 +0600

    Extracted complex views into ViewBuilders

--- Shared/Settings/MenuEditor/Add to Menu/AddToMenuView/MenuEditorView.swift
+++ Shared/Settings/MenuEditor/Add to Menu/AddToMenuView/MenuEditorView.swift
@@ -22,37 +22,38 @@ struct MenuEditorView: View {
     @State private var itemAlreadyExists        = false
     
     @State private var newItem: MenuItem
-    private var n: Bool
-    private var originalName: String = ""
+    private var item_editing_mode: Bool
+    private var original_name: String = ""
     
+    /// `EnvironmentObject` that contains information about items and ingredients on the menu.
     @EnvironmentObject var menuItemStore: MenuItemStore
     
     init() {
         self._newItem = State(initialValue: MenuItem())
-        self.n = true
+        self.item_editing_mode = true
     }
     
     init(menuItem: MenuItem) {
         self._newItem = State(initialValue: menuItem)
-        self.n = false
-        self.originalName = menuItem.itemName
-        print(originalName)
+        self.item_editing_mode = false
+        self.original_name = menuItem.itemName
+        print(original_name)
     }
     
-    // MARK: - Body
     var body: some View {
+        // MARK: - List
         List {
-            
-            // MARK: itemName
             Section {
+                
+                // MARK: itemName
                 TextField("Name", text: $newItem.itemName)
                     .textInputAutocapitalization(.words)
                     .focused($focusField, equals: .itemName)
                     .submitLabel(.done)
-                    .onSubmit { focusField = .regularIngredients }
+                    .onSubmit(addNewRegularIngredient)
                     .onChange(of: newItem.itemName, perform: { it in
                         let item_name = it.trimmingCharacters(in: .whitespacesAndNewlines).lowercased()
-                        itemAlreadyExists = menuItemStore.items.contains(where: { item_name == $0.itemName.lowercased() }) && item_name != originalName.lowercased()
+                        itemAlreadyExists = menuItemStore.items.contains(where: { item_name == $0.itemName.lowercased() }) && item_name != original_name.lowercased()
                     })
                 
             } footer: {
@@ -64,15 +65,13 @@ struct MenuEditorView: View {
             // MARK: Regular ingredients
             Section(content: {
                 ForEach(newItem.regularIngredients.indices, id: \.self) {
-                    TextField("Ingredient Name", text: $newItem.regularIngredients[$0])
+                    
+                    text_field($newItem.regularIngredients[$0])
                         .focused($focusField, equals: newItem.regularIngredients.last == "" ? .regularIngredients : .nil)
-                        .submitLabel(.next)
-                        .autocapitalization(.words)
                         .onSubmit(addNewRegularIngredient)
-                        .onTapGesture { purgeList() }.deleteDisabled(newItem.regularIngredients[$0] == "")
+                    
                 }.onDelete { newItem.regularIngredients.remove(atOffsets: $0) }
                 
-                // MARK: Add regular button
                 Button("Add ingredient...", action: addNewRegularIngredient)
                 
             }, header: {
@@ -83,16 +82,13 @@ struct MenuEditorView: View {
             // MARK: Extra ingredients
             Section(content: {
                 ForEach(newItem.extraIngredients.indices, id: \.self) {
-                    TextField("Ingredient Name", text: $newItem.extraIngredients[$0])
+                    
+                    text_field($newItem.extraIngredients[$0])
                         .focused($focusField, equals: newItem.extraIngredients.last == "" ? .extraIngredients : .nil)
-                        .submitLabel(.next)
-                        .autocapitalization(.words)
                         .onSubmit(addNewExtraIngredient)
-                        .onTapGesture { purgeList() }
-                        .deleteDisabled(newItem.extraIngredients[$0] == "")
+                    
                 }.onDelete { newItem.extraIngredients.remove(atOffsets: $0) }
                 
-                // MARK: Add extra button
                 Button("Add ingredient...", action: addNewExtraIngredient)
                 
             }, header: {
@@ -101,38 +97,10 @@ struct MenuEditorView: View {
             
             // MARK: - Warnings
             Section(content: {
-                /**
-                 Over here, two separate arrays are being used: one, `menuItemStore.warnings` that is passed from the previous view and the other, `newItem.warnings` that is used to keep track of the items that have been selected.
-                 First, the `menuItemStore.warnings` list is looped through to display all possible warnings that the app has stored on device.
-                 When the button is clicked, the algorithm checks if the item clicked upon exists in `newItem.warnings`.
-                 If it isn't, then the item is added to `newItem.warnings` and a checkmark is displayed.
-                 If it is present inside `newItem.warnings`, the item is removed upon click and the checkmark is removed.
-                 */
                 ForEach(menuItemStore.warnings, id: \.self) { each_warning in
-                    Button (action: {
-                        if newItem.warnings.contains(each_warning) {
-                            if let index = newItem.warnings.firstIndex(of: each_warning) {
-                                newItem.warnings.remove(at: index)
-                            }
-                        } else {
-                            newItem.warnings.append(each_warning)
-                        }
-                    }) {
-                        // MARK: Warning selectors
-                        HStack {
-                            Text(each_warning.capitalized)
-                                .foregroundColor(.sensiBlack)
-                            
-                            Spacer()
-                            
-                            Image(systemName: "checkmark")
-                                .padding(.trailing, 5)
-                                .opacity(newItem.warnings.contains(each_warning) ? 1 : 0)
-                        }
-                    }
+                    warning_selector_label(label: each_warning)
                 }
                 
-                // MARK: EditWarnings button
                 Button("Edit warnings...") {
                     showWarningsEditor = true
                 }
@@ -143,72 +111,135 @@ struct MenuEditorView: View {
             
             // MARK: - Category Picker
             Section(content: {
-                Button {
-                    showCategoryPickerView = true
-                } label: {
-                    HStack {
-                        Text("Category")
-                            .foregroundColor(.sensiBlack)
-                        Spacer()
-                        Text(newItem.category)
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(.gray)
-                            .font(.caption)
-                    }
-                }
+                category_picker_label()
             }, header: {
                 Text("Category")
             })
             
-            // MARK: Delete button
-            if !n {
-                Button("Delete Item", role: .destructive) {
-                    if let index = menuItemStore.items.firstIndex(where: { $0.id == newItem.id }) {
-                        menuItemStore.items.remove(at: index)
-                    }
-                    dismiss()
-                }.frame(maxWidth: .infinity)
+            
+            // MARK: - Delete button
+            Section {
+                delete_button()
             }
             
-            // MARK: - End Form
-        }.navigationTitle(n ? "Add Item to Menu" : "Edit Menu Item")
+            // MARK: - End List
+        }.navigationTitle(item_editing_mode ? "Add Item to Menu" : "Edit Menu Item")
             .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .confirmationAction) {
-                    // MARK: Toolbar 'Save' button
-                    Button("Save") {
-                        showConfirmationDialog = newItem.regularIngredients.isEmpty || newItem.extraIngredients.isEmpty
-                        if !showConfirmationDialog {
-                            saveItemsData()
-                            dismiss()
-                        }
-                    }.disabled(newItem.itemName.isEmpty || newItem.category.isEmpty || itemAlreadyExists )
-                }
-            }.sheet(isPresented: $showCategoryPickerView, content: {
-                // MARK: CategoryPicker Sheet
-                CategoryPickerView(selection: $newItem.category)
-                    .environmentObject(menuItemStore)
-            })
+            .toolbar { ToolbarItem(placement: .confirmationAction) { save_button() } }
+            .sheet(isPresented: $showCategoryPickerView, content: { CategoryPickerView(selection: $newItem.category).environmentObject(menuItemStore)})
             .interactiveDismissDisabled()
-            .sheet(isPresented: $showWarningsEditor) {
-                // MARK: WarningEditor Sheet
-                WarningsEditorView().environmentObject(menuItemStore)
-                
-            }
+            .sheet(isPresented: $showWarningsEditor) { WarningsEditorView().environmentObject(menuItemStore) }
             .confirmationDialog("Discard Changes", isPresented: $showConfirmationDialog, actions: {
-                
-                // MARK: Confirmation Dialog
-                Button("Keep Editing", role: .cancel) { }
-                Button("Save anyway") {
-                    saveItemsData()
-                    dismiss()
-                }
+                confirmation_dialog_actions()
             }, message: {
                 Text("You haven't added any ingredients to your list")
             })
     }
-    // MARK: - End Body
     
+    
+    
+    // MARK: - ViewBuilders
+    /// **Save button** that is displayed in the toolbar.
+    @ViewBuilder private func save_button() -> some View {
+        Button("Save") {
+            showConfirmationDialog = newItem.regularIngredients.isEmpty || newItem.extraIngredients.isEmpty
+            if !showConfirmationDialog {
+                saveItemsData()
+                dismiss()
+            }
+        }.disabled(newItem.itemName.isEmpty || newItem.category.isEmpty || itemAlreadyExists )
+    }
+    
+    /// **Delete Button** that deletes item in editing more from ``menuItemStore`` that is displayed at the end of the form only in editing mode.
+    ///
+    /// Checks if current view is in editing more before appearing in the form.
+    @ViewBuilder private func delete_button() -> some View {
+        if !item_editing_mode {
+            Button("Delete Item", role: .destructive) {
+                if let index = menuItemStore.items.firstIndex(where: { $0.id == newItem.id }) {
+                    menuItemStore.items.remove(at: index)
+                }
+                dismiss()
+            }.frame(maxWidth: .infinity)
+        }
+    }
+    
+    /// **TextField** where the user can edit or enter new ingredients.
+    @ViewBuilder func text_field(_ text: Binding<String>) -> some View {
+        TextField("Ingredient Name", text: text)
+            .submitLabel(.next)
+            .autocapitalization(.words)
+            .onTapGesture { purgeList() }
+            .deleteDisabled(text.wrappedValue == "")
+    }
+    
+    /// Clickable label that opens `sheet`to view `CategoryPickerView`.
+    @ViewBuilder func category_picker_label() -> some View {
+        Button {
+            showCategoryPickerView = true
+        } label: {
+            HStack {
+                Text("Category")
+                    .foregroundColor(.sensiBlack)
+                Spacer()
+                Text(newItem.category)
+                Image(systemName: "chevron.right")
+                    .foregroundColor(.gray)
+                    .font(.caption)
+            }
+        }
+        
+    }
+    
+    /// Confirmation dialog actions.
+    ///
+    /// Actions:
+    /// - **Keep Editing**
+    /// - **Save Anyway**
+    @ViewBuilder func confirmation_dialog_actions() -> some View {
+        Button("Keep Editing", role: .cancel) { }
+        Button("Save anyway") {
+            saveItemsData()
+            dismiss()
+        }
+    }
+    
+    /**
+     Clickable label for displaying warnings.
+     
+     Ttwo separate arrays are being used:
+     - `menuItemStore.warnings` that stores all previously added warnings.
+     - `newItem.warnings` that is used to keep track of selected warnings.
+     
+     A loop runs through `menuItemStore.warnings` to display all previously saved warnings.
+     
+     Upon click on `newItem.warnings`, if clicked warning label is contained in `newItem.warnings` it is removed.
+     Else the warning is added into the list.
+     */
+    
+    @ViewBuilder func warning_selector_label(label each_warning: String) -> some View {
+        Button (action: {
+            if newItem.warnings.contains(each_warning) {
+                if let index = newItem.warnings.firstIndex(of: each_warning) {
+                    newItem.warnings.remove(at: index)
+                }
+            } else {
+                newItem.warnings.append(each_warning)
+            }
+        }) {
+            // MARK: Warning selectors
+            HStack {
+                Text(each_warning.capitalized)
+                    .foregroundColor(.sensiBlack)
+                
+                Spacer()
+                
+                Image(systemName: "checkmark")
+                    .padding(.trailing, 5)
+                    .opacity(newItem.warnings.contains(each_warning) ? 1 : 0)
+            }
+        }
+    }
     
     
     // MARK: enum FocusField
@@ -222,9 +253,11 @@ struct MenuEditorView: View {
     // MARK: - func Save data
     
     /// Appends `newItem` to `environmentObject` ``menuItemStore``.
+    ///
+    /// Checks if a new item is being entered and appropriately adds or replaces the item in ``menuItemStore``.
     private func saveItemsData() {
         purgeList()
-        if n {
+        if item_editing_mode {
             menuItemStore.items.append(newItem)
         } else if let index = menuItemStore.items.firstIndex(where: { $0.id == newItem.id }) {
             menuItemStore.items.insert(newItem, at: index)
