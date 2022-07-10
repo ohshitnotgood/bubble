e7998abd483d037fbc30a40a63024a6f98aedee6
ohshitnotgood <thumbs.exile_0g@icloud.com>
2022-07-11 00:15:26 +0600

    Added alphabetical scrollbar

--- Shared/ItemsView/AlphabeticItemsView.swift
+++ Shared/ItemsView/AlphabeticItemsView.swift
@@ -22,6 +22,10 @@ struct AlphabeticItemsView: View {
     
     @State private var alphabets: [String] = []
     
+    init() {
+        UITableView.appearance().showsVerticalScrollIndicator = false
+    }
+    
     private func makeAlphabetsList() {
         for each_item in menuItemStore.items {
             if let firstChar = each_item.itemName.first {
@@ -34,18 +38,81 @@ struct AlphabeticItemsView: View {
     
     // MARK: Body
     var body: some View {
-        List {
-            ForEach(alphabets, id: \.self) { alphabet in
-                Section(header: Text(alphabet), content: {
-                    ForEach(menuItemStore.items.filter { $0.itemName.hasPrefix(alphabet)}, id: \.self) { menuItem in
-                        ListItemCellView(menuItem)
+        ScrollViewReader { reader in
+            ZStack {
+                List {
+                    ForEach(alphabets, id: \.self) { alphabet in
+                        Section(header: Text(alphabet), content: {
+                            ForEach(menuItemStore.items.filter { $0.itemName.hasPrefix(alphabet)}, id: \.self) { menuItem in
+                                ListItemCellView(menuItem)
+                            }
+                        }).id(alphabet)
                     }
-                })
+                }
+                AlphabetsScrollBar(reader, alphabetsInView: alphabets)
             }
-        }.interactiveDismissDisabled()
+        }
             .listStyle(.plain)
-            .searchable(text: $searchText)
+            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
             .onAppear(perform: makeAlphabetsList)
+    }
+}
+
+struct AlphabetsScrollBar: View {
+    var alphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    var reader: ScrollViewProxy
+    var alphabetsInView: [String]
+    let generator = UINotificationFeedbackGenerator()
+    
+    @GestureState private var dragLocation: CGPoint = .zero
+    
+    
+    init(_ reader: ScrollViewProxy, alphabetsInView: [String]) {
+        self.reader = reader
+        self.alphabetsInView = alphabetsInView
+    }
+    
+    var body: some View {
+        VStack {
+            ForEach(alphabets, id: \.self) { each_a in
+                VStack {
+                    Text(each_a).onTapGesture {
+                        reader.scrollTo(each_a)
+                        if alphabets.contains(each_a) {
+                            generator.notificationOccurred(.warning)
+                        }
+                    }.padding(.leading)
+                }
+                .buttonStyle(.plain)
+                .font(.caption2.weight(.semibold))
+                .foregroundColor(.accentColor)
+                .background {
+                    GeometryReader { geometry in
+                        dragObserver(geometry: geometry, scrollTo: each_a)
+                    }
+                }
+            }
+        }
+        .padding(5)
+        .frame(maxWidth: .infinity, alignment: .trailing)
+        .gesture (
+            DragGesture(minimumDistance: 0, coordinateSpace: .global)
+                .updating($dragLocation) { value, drag_location, _ in
+                    print("Drag detected at \(value.location)")
+                    drag_location = value.location
+                }
+        )
+    }
+    
+    func dragObserver(geometry: GeometryProxy, scrollTo: String) -> some View {
+        if geometry.frame(in: .global).contains(dragLocation) {
+            DispatchQueue.main.async {
+                print("Scrolling to on drag")
+                generator.notificationOccurred(.success)
+                reader.scrollTo(scrollTo, anchor: .center)
+            }
+        }
+        return Rectangle().foregroundStyle(.background)
     }
 }
 
